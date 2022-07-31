87ecfb528cceccda38dca874a65bdf0497afe08f
magnetoasher <magnetoasher@gmail.com>
2022-07-31 01:33:56 -0700

    feat: added signup permission

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -1,8 +1,13 @@
-import { rule, shield } from 'graphql-shield';
+import { or, rule, shield } from 'graphql-shield';
+
 import { getUserId } from '@/utils';
 import { Context } from '@/context';
 
 const rules = {
+  isGuest: rule()((_parent, _args, context: Context) => {
+    const userId = getUserId(context);
+    return Boolean(!userId);
+  }),
   isAuthenticatedUser: rule()((_parent, _args, context: Context) => {
     const userId = getUserId(context);
     return Boolean(userId);
@@ -18,16 +23,22 @@ const rules = {
   }),
 };
 
-export const permissions = shield({
-  Query: {
-    draftsByUser: rules.isAuthenticatedUser,
-    postById: rules.isAuthenticatedUser,
+export const permissions = shield(
+  {
+    Query: {
+      draftsByUser: rules.isAuthenticatedUser,
+      postById: or(rules.isAuthenticatedUser, rules.isGuest),
+    },
+    Mutation: {
+      signup: rules.isGuest,
+      changePassword: rules.isAuthenticatedUser,
+      createDraft: rules.isAuthenticatedUser,
+      deletePost: rules.isPostOwner,
+      incrementPostViewCount: rules.isAuthenticatedUser,
+      togglePublishPost: rules.isPostOwner,
+    },
   },
-  Mutation: {
-    changePassword: rules.isAuthenticatedUser,
-    createDraft: rules.isAuthenticatedUser,
-    deletePost: rules.isPostOwner,
-    incrementPostViewCount: rules.isAuthenticatedUser,
-    togglePublishPost: rules.isPostOwner,
+  {
+    allowExternalErrors: true,
   },
-});
+);
